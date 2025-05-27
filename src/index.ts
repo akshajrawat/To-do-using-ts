2d55974daf0299e1669c0dc5eb23129b85245102
Akshaj Rawat <akshajr11@gmail.com>
2025-05-27 20:56:53 +0530

    Complete todo list app using typescripts

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,12 @@ const taskInput = document.getElementById(
 ) as HTMLInputElement | null;
 const addTaskBtn = document.getElementById("addTaskBtn");
 const taskList = document.getElementById("taskList") as HTMLUListElement | null;
+const filterAll = document.getElementById(
+  "filterAll"
+) as HTMLButtonElement | null;
+const filterCompleted = document.getElementById("filterCompleted");
+const filterActive = document.getElementById("filterActive");
+const resetAll = document.getElementById("resetAll");
 
 // defining interface
 interface Task {
@@ -15,7 +21,7 @@ interface Task {
 // Defining
 type filter = "All" | "Active" | "Completed";
 let tasks: Task[] = [];
-let currentFilter: filter = "All";
+let currentFilter: filter;
 
 // load tasks
 function loadTasks(): void {
@@ -23,11 +29,34 @@ function loadTasks(): void {
   if (saved) {
     tasks = JSON.parse(saved);
   }
+  let loadFilter = localStorage.getItem("filter");
+  if (loadFilter === "Completed") {
+    currentFilter = "Completed";
+  } else if (loadFilter === "Active") {
+    currentFilter = "Active";
+  } else {
+    currentFilter = "All";
+  }
 }
 
 // save to local storage
 function saveTasks(): void {
   localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
+// Filter
+function filterRemover(): void {
+  filter.forEach((item) => {
+    if (!item) return;
+    item?.classList.remove("bg-blue-500", "text-white");
+    item?.classList.add("bg-gray-300", "dark:bg-gray-700");
+  });
+}
+
+function filterClassHandler(active: HTMLElement | null): void {
+  filterRemover();
+  active?.classList.remove("bg-gray-300", "dark:bg-gray-700");
+  active?.classList.add("bg-blue-500", "text-white");
 }
 
 // render task
@@ -37,10 +66,15 @@ function renderTask(): void {
   let filteredTask = tasks;
 
   if (currentFilter === "Active") {
+    filterClassHandler(filterActive);
     filteredTask = tasks.filter((t) => !t.completed);
   } else if (currentFilter === "Completed") {
+    filterClassHandler(filterCompleted);
     filteredTask = tasks.filter((t) => t.completed);
+  } else {
+    filterClassHandler(filterAll);
   }
+
   filteredTask.forEach((task) => {
     const list: HTMLElement = document.createElement("li");
     const span: HTMLElement = document.createElement("span");
@@ -62,7 +96,7 @@ function renderTask(): void {
     button.textContent = "Delete";
 
     button.addEventListener("click", () => {
-      tasks = tasks.filter((t) => t.id !== task.id);
+      task.completed = true;
       saveTasks();
       renderTask();
     });
@@ -82,7 +116,6 @@ function addTask(): void {
   if (!taskInput) {
     return;
   }
-
   const value: string = taskInput.value;
 
   if (value.trim() === "") {
@@ -97,9 +130,48 @@ function addTask(): void {
 
   saveTasks();
   renderTask();
-
   taskInput.value = "";
 }
 
 addTaskBtn?.addEventListener("click", addTask);
+
+// Changing Filter
+const filter: (HTMLElement | null)[] = [
+  filterAll,
+  filterActive,
+  filterCompleted,
+];
+
+function filterHandler(active: HTMLElement | null) {
+  filterClassHandler(active);
+  if (active === filterCompleted) {
+    currentFilter = "Completed";
+    renderTask();
+  } else if (active === filterActive) {
+    currentFilter = "Active";
+    renderTask();
+  } else {
+    currentFilter = "All";
+    renderTask();
+  }
+  localStorage.setItem("filter", currentFilter);
+}
+
+filterAll?.addEventListener("click", () => filterHandler(filterAll));
+
+filterActive?.addEventListener("click", () => filterHandler(filterActive));
+
+filterCompleted?.addEventListener("click", () =>
+  filterHandler(filterCompleted)
+);
+
+// reset all
+resetAll?.addEventListener("click", () => {
+  localStorage.removeItem("tasks");
+  tasks = [];
+  renderTask();
+});
+
+// Calling
+loadTasks();
 renderTask();
